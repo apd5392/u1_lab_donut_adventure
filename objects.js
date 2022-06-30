8049e83aa427b3910de177f295d7768826e8c10b
apd5392 <adoumbia.pas@gmail.com>
2022-06-30 15:30:54 -0400

    Hamster class created

---
+++ objects.js
@@ -0,0 +1,10 @@
+class Hamster{
+    constructor(owner, name, price){
+        this.owner = owner;
+        this.name = name;
+        this.price = price;
+    }
+    wheelRun(){};
+    eatFood(){};
+    getPrice(){};
+}
